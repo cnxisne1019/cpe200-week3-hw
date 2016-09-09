a508883f98d95597bfc310376ba47e2019869315
teromizer <teromizer@gmail.com>
2016-09-09 12:37:08 +0700

    edited.

--- src/cpe200/Course.java
+++ src/cpe200/Course.java
@@ -10,16 +10,25 @@ public class Course {
     }
 
     public Course(String n, String cid) {
-        // implement here
+        course_id="000000";
+        course_name="TBA";
+        lecturer="TBA";
+        no_students=0;
+        max_students=30;
     }
 
     public Course(String n, String cid, String l) {
-        // implement here
+        course_id=n;
+        course_name=n;
+        lecturer=l;
     }
 
     public Course(String n, String cid, String l, int max) {
         this.course_name = !n.equalsIgnoreCase("")?n:"TBA";
-        // implement the rest here
+        this.course_id = isValidCourse_id(cid)?cid:"000000";
+        this.lecturer = !l.equalsIgnoreCase(" ")?l:"TBA";
+        this.max_students=(max >= 10)?max:30;
+        this.no_students = 0;
     }
 
     public String getCourse_name() {
@@ -40,50 +49,60 @@ public class Course {
 
     // implement the other get and set methods here
     public String getLecturer() {
-        // implement here
         return "lecturer";
     }
 
     public void setLecturer(String lecturer) {
-        // implement here
+        this.lecturer = !lecturer.equalsIgnoreCase(" ")?lecturer:this.lecturer;
     }
 
-    public int getMax_students() {
-        // implement here
-        return 0;
+    public int getMax_students(){
+        return this.max_students;
     }
 
     public void setMax_students(int max_students) {
-        // implement here
+        this.max_students = (max_students >= 10)?max_students:60;
     }
 
     public int getNo_students() {
-        // implement here
         return 0;
     }
 
     public void setNo_students(int no_students) {
-        // implement here
+        this.no_students = no_students;
     }
 
     @Override
     public String toString() {
-        String o = this.course_name + " ("
-                + this.course_id + "), Teacher: "
-                + this.lecturer + ", has ";
+        if (no_students == 0) {
+            String o = this.course_name + " ("
+                    + this.course_id + "), Teacher: "
+                    + this.lecturer + ", has "
+                    + "NO student, "
+                    + "[maximum: 60]";
+            return o;
+        } else if (no_students == 1) {
+            String o = this.course_name + " ("
+                    + this.course_id + "), Teacher: "
+                    + this.lecturer + ", has "
+                    + "ONE student, "
+                    + "[maximum: 60]";
+            return o;
+        } else {
+            String o = this.course_name + " ("
+                    + this.course_id + "), Teacher: "
+                    + this.lecturer + ", has "
+                    + this.no_students + " students, "
+                    + "[maximum: 60]";
+            return o;
+        }
 
-        // implement the rest here
-
-        return o;
     }
 
     private boolean isValidCourse_id(String id) {
         Pattern p = Pattern.compile(idREGEX);
         Matcher m = p.matcher(id);
-
-        // implement the rest here
-
-        return true;
+        return m.matches();
     }
 
     // Regular expression for the Student ID pattern
